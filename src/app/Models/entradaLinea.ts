1afcb6bd1b80e84e7f232f1d6796fd62a05c0a05
Jose Garcia <josegarcia.developer@gmail.com>
2020-11-17 19:04:09 -0400

    faltaron los modelos de entrada linea y de linea

---
+++ src/app/Models/entradaLinea.ts
@@ -0,0 +1,8 @@
+export interface EntradaLinea{
+    nro_orden?:number | string;
+    id_linea?:number | string;
+    id_lineaprod?:number | string;
+    fecha?:Date;
+    cantidad?:number | string;
+    id_empleado?:string;
+}
---
+++ src/app/Models/lineaprod.ts
@@ -0,0 +1,22 @@
+export interface LineaProd{
+    id_lineaprod?:number | string;
+    id_linea?:number | string;
+    id_prodpro?:number | string;
+    existencia?:number;
+    id_producto:number | string;
+    id_proveedor:number | string;
+    codigo_producto:number | string;
+    nombre_producto:number | string;
+    unidad_medida:number | string;
+    nombre_proveedor:number | string;
+    descripcion_prov:number | string;
+    nombre_linea:number | string;
+    id_departamento:number | string;
+    descripcion_linea:number | string;
+    nombre_departamento:number | string;
+    descripcion_dep:number | string;
+   
+}
+
+        
+        
